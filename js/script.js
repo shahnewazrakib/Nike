959e37a546c7f1a8669b6df24a88d81ec10f3151
Shhanewaz Rakib <personal.shahnewazrakib@gmail.com>
2023-10-11 11:10:14 +0600

    Website completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const header = document.querySelector("header");
+const nav = document.getElementById("nav");
+const menu__bar = document.querySelector(".fa-bars");
 const price = document.querySelector(".hero__section .price");
 const hero__image = document.querySelector(".hero__section .shoe img");
 const shoe__cards = document.querySelectorAll(".shoe__card");
@@ -12,34 +15,41 @@ shoe__cards.forEach((card) => {
     card.classList.add("active");
     hero__image.src = image__source;
     price.innerText = shoe__price;
+    window.scrollTo({ top: 0 });
   });
 });
 
-const countDownDate = new Date("2023-12-01 00:00:00");
+menu__bar.addEventListener("click", () => {
+  nav.classList.toggle("show");
+  if (nav.classList.contains("show")) {
+    menu__bar.className = "fa-regular fa-circle-xmark";
+  } else {
+    menu__bar.className = "fa-solid fa-bars";
+  }
+});
 
-// Calculate the time remaining
+const countDownDate = new Date("2023-12-01 00:00:00");
 let timeRemaining = countDownDate - new Date();
 
-// Convert the time remaining to days, hours, minutes, and seconds
 let days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-let hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+let hours = Math.floor(
+  (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+);
 let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
 let seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-// Update the countdown timer every second
-setInterval(function() {
-
-  // Calculate the time remaining
+setInterval(function () {
   timeRemaining = countDownDate - new Date();
 
-  // Convert the time remaining to days, hours, minutes, and seconds
   days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-  hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  hours = Math.floor(
+    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
   minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
   seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-  document.getElementById('days').textContent = days;
-  document.getElementById('hours').textContent = hours;
-  document.getElementById('minutes').textContent = minutes;
-  document.getElementById('seconds').textContent = seconds;
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
 }, 1000);
